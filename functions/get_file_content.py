71d97964d2052a6ca8ef7b955cdeafade2e99c16
ericksotoe <ericksotoe@gmail.com>
2025-11-11 22:45:30 +0900

    added functionality to read files given a directory and filepath

---
+++ functions/get_file_content.py
@@ -0,0 +1,30 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    # gives us the target path by combining working and the file_path
+    full_tar_path = os.path.join(working_directory, file_path)
+    
+    # bot 5 lines are used to check if the abs_target is inside the abs_working file path.
+    abs_working = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(full_tar_path)
+    prefix = abs_working if abs_working.endswith(os.sep) else abs_working + os.sep
+    if not (abs_target == abs_working or abs_target.startswith(prefix)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    # checks to see if the provided directory is indeed a file
+    if not os.path.isfile(abs_target):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    # prints out the formatted text
+    try:
+        with open(abs_target, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(abs_target) > MAX_CHARS:
+                file_content_string = file_content_string + f'[...File "{file_path}" truncated at 10000 characters]'
+
+            return file_content_string
+
+    except Exception as e:
+        return f"Error: {e}"
