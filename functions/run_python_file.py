d12620d9e1891f054834c0e99bd105fffd716f5c
ericksotoe <ericksotoe@gmail.com>
2025-11-12 23:36:34 +0900

    can now run python files

---
+++ functions/run_python_file.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    # gives us the target path by combining working directory and the file_path
+    full_tar_path = os.path.join(working_directory, file_path)
+    
+    # bot 5 lines are used to check if the abs_target is inside the abs_working file path.
+    abs_working = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(full_tar_path)
+    prefix = abs_working if abs_working.endswith(os.sep) else abs_working + os.sep
+    if not (abs_target == abs_working or abs_target.startswith(prefix)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        # checks to see if the provided file path exists if not create it and write to it
+        if not os.path.exists(abs_target):        
+            return f'Error: File "{file_path}" not found.'
+        
+        # checks to see if the file is an executable python file
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+        
+        # provided file path exists and isnt a dir, overwrite it
+        else:
+            result = subprocess.run(["python3", abs_target, *args], capture_output=True, text=True, timeout=30, check=True)
+            # formatting depending on the returned object
+            output = []
+            if result.stdout:
+                output.append(f"STDOUT:\n{result.stdout}")
+            if result.stderr:
+                output.append(f"STDERR:\n{result.stderr}")
+
+            if result.returncode != 0:
+                output.append(f"Process exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+
+        
+    except Exception as e:
+        return f'Error: executing Python file: {e}'
--- tests.py
+++ tests.py
@@ -1,44 +1,84 @@
+from functions.run_python_file import run_python_file
 # from functions.get_files_info import get_files_info
-
-# print("Result for current directory:")
-# print(get_files_info("calculator", "."))
-
-# print("Result for 'pkg' directory:")
-# print(get_files_info("calculator", "pkg"))
-
-# print("Result for '/bin' directory:")
-# print(get_files_info("calculator", "/bin"))
-
-# print("Result for '../' directory:")
-# print(get_files_info("calculator", "../"))
-
 # from functions.get_file_content import get_file_content
+# from functions.write_file import write_file
 
-# # print("Result from getting lorem.txt info")
-# # print(get_file_content("calculator", "lorem.txt"))
 
-# print('get_file_content("calculator", "main.py")')
-# print(get_file_content("calculator", "main.py"))
+def test():
 
-# print('get_file_content("calculator", "pkg/calculator.py")')
-# print(get_file_content("calculator", "pkg/calculator.py"))
+    # print("Result for current directory:")
+    # print(get_files_info("calculator", "."))
 
-# print('get_file_content("calculator", "/bin/cat")')
-# print(get_file_content("calculator", "/bin/cat"))
+    # print("Result for 'pkg' directory:")
+    # print(get_files_info("calculator", "pkg"))
 
-# print('get_file_content("calculator", "pkg/does_not_exist.py")')
-# print(get_file_content("calculator", "pkg/does_not_exist.py"))
+    # print("Result for '/bin' directory:")
+    # print(get_files_info("calculator", "/bin"))
 
-from functions.write_file import write_file
+    # print("Result for '../' directory:")
+    # print(get_files_info("calculator", "../"))
 
-print('==========================================================')
-print('write_file("calculator", "lorem.txt", "wait, this isnt lorem ipsum")')
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
-print('==========================================================')
-print('write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")')
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
-print('==========================================================')
-print('write_file("calculator", "/tmp/temp.txt", "this should not be allowed")')
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+    # # print("Result from getting lorem.txt info")
+    # # print(get_file_content("calculator", "lorem.txt"))
+
+    # print('get_file_content("calculator", "main.py")')
+    # print(get_file_content("calculator", "main.py"))
+
+    # print('get_file_content("calculator", "pkg/calculator.py")')
+    # print(get_file_content("calculator", "pkg/calculator.py"))
+
+    # print('get_file_content("calculator", "/bin/cat")')
+    # print(get_file_content("calculator", "/bin/cat"))
+
+    # print('get_file_content("calculator", "pkg/does_not_exist.py")')
+    # print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+
+
+
+
+    # print('==========================================================')
+    # print('write_file("calculator", "lorem.txt", "wait, this isnt lorem ipsum")')
+    # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+
+    # print('==========================================================')
+    # print('write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")')
+    # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+
+    # print('==========================================================')
+    # print('write_file("calculator", "/tmp/temp.txt", "this should not be allowed")')
+    # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+
+
+    print('=========Test 1=============')
+    print('run_python_file("calculator", "main.py")')
+    print(run_python_file("calculator", "main.py"))
+
+    print('=========Test 2=============')
+    print('run_python_file("calculator", "main.py", ["3 + 5"])')
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+
+    print('=========Test 3=============')
+    print('run_python_file("calculator", "tests.py")')
+    print(run_python_file("calculator", "tests.py"))
+
+    print('==========Test 4============')
+    print('run_python_file("calculator", "../main.py")')
+    print(run_python_file("calculator", "../main.py"))
+
+    print('==========Test 5============')
+    print('run_python_file("calculator", "nonexistent.py")')
+    print(run_python_file("calculator", "nonexistent.py"))
+
+    print('==========Test 6============')
+    print('run_python_file("calculator", "lorem.txt")')
+    print(run_python_file("calculator", "lorem.txt"))
+
+
+if __name__ == "__main__":
+    test()
+
